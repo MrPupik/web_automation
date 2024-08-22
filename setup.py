1896598f9c54d8f51f8ed780c67223f7547cfc19
Itay Zohar <itaynzohar@gmail.com>
2024-08-22 19:43:02 +0300

    fix pack

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name="web_automation",
     version="0.1",
-    # packages=[
-    #     "web_automation",
-    # ],
+    packages=[
+        "web_automation",
+    ],
     license="MIT",
-    # package_dir={
-    #     "selenium_wrapper": ".",
-    # },
+    package_dir={
+        "web_automation": ".",
+    },
     long_description="Web Automation Tools"
 )
